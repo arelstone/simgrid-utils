adfb0a707e82278790fc385d53e83162c6f8399d
Carsten Daurehøj <arelstone@gmail.com>
2022-06-17 11:02:49 +0200

    🚨 Linting

--- src/csv.ts
+++ src/csv.ts
@@ -1,13 +1,14 @@
-import { CsvEntry, ID } from "./types";
-import { fetch } from './fetch'
-import { parse } from "papaparse";
+import { CsvEntry, ID } from './types';
+import { fetch } from './fetch';
+import { parse } from 'papaparse';
 
 export const csv = async (id: ID): Promise<ReadonlyArray<CsvEntry>> => {
-  return parse<CsvEntry>(
-    await fetch<string>(id, 'csv', { responseType: 'blob' }),
-    { header: true 
-  })
-  .data
-  .filter(({ username }) => !!username)
-}
+    return parse<CsvEntry>(
+        await fetch<string>(id, 'csv', { responseType: 'blob' }),
+        {
+            header: true,
+        })
+        .data
+        .filter(({ username }) => !!username);
+};
 
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,20 +1,20 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { StatusCodes } from "http-status-codes";
-import { EntryListType, ID } from "./types";
+import axios, { AxiosRequestConfig } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import { EntryListType, ID } from './types';
 
 export const fetch =async <T = any>(
-  id: ID,
-  filetype: EntryListType,
-  options: AxiosRequestConfig = {},
+    id: ID,
+    filetype: EntryListType,
+    options: AxiosRequestConfig = {},
 ): Promise<T> =>  {
-  const { data, status, statusText } = await axios.get(
-    `https://www.thesimgrid.com/admin/championships/${id}/registrations.${filetype}`,
-    options,
-  );
+    const { data, status, statusText } = await axios.get(
+        `https://www.thesimgrid.com/admin/championships/${id}/registrations.${filetype}`,
+        options,
+    );
 
-  if (status !== StatusCodes.OK) {
-    throw new Error(`${status}: ${statusText}`);
-  }
+    if (status !== StatusCodes.OK) {
+        throw new Error(`${status}: ${statusText}`);
+    }
 
-  return data;
-}
+    return data;
+};
